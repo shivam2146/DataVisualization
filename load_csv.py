3dfb545e03c797405226feec461cad8379a24a79
Shivam Agarwal <shivam.mcs17.du@gmail.com>
2018-01-17 01:12:38 +0530

    read data set from url and csv files

---
+++ load_csv.py
@@ -0,0 +1,23 @@
+from pandas import read_csv
+def test():
+    url="https://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt"
+    filename= "iris.csv"
+    names=['var','skewness','curtosis','entropy','class']
+    '''
+    using url from where to load dataset
+    '''
+    data= read_csv(url,names=names)     #reads csv file into a data frame or text parser
+    data1 = read_csv(url,header=None)  #as header is none it doesn't takes first row as column names it assigns names itself
+    data2=read_csv(url)             #there is a argument index_col which can be used to assign a column as index
+    print(data.head(5),"\n")
+    print(data1.head(5),"\n")
+    print(data2.head(5),"\n")
+
+    '''
+    using csv file to load data
+    '''
+    data3= read_csv(filename,skiprows=[1])          #skips row no 1 as it contains datatypes and we dont need em
+    print(data3.head(5))
+
+if __name__=='__main__':
+    test()
