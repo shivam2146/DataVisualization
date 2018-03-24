62e6fb2deb2a1e82a27f7107220be48c192078ac
shivam2146 <shivam.mcs17.du@gmail.com>
2018-03-25 00:41:05 +0530

    add stats for dataframe

---
+++ stats_df.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from matplotlib import pyplot
+import numpy as np
+def test():
+    filename= "iris.csv"
+    df = pd.read_csv(filename,skiprows=[1])
+    #print(df.describe())
+    print("max:",df["sepallength"].max())
+    print("count:",df["sepallength"].count())
+    print("mad:",df["sepallength"].mad())
+    print("median:",df["sepallength"].median())
+    print("min:",df["sepallength"].min())
+    print("mean:",df["sepallength"].mean())
+    print("s.d:",df["sepallength"].std())
+    print("var:",df["sepallength"].var())
+    print("skew:",df["sepallength"].skew())
+    print("kurt:",df["sepallength"].kurt())
+
+
+
+
+
+
+if __name__ == '__main__':
+        test()
